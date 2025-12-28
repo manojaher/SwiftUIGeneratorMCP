fa7bf38f328e7d975bf65e21cbfd9c6430f3a29c
Manoj Aher <aher.manoj@outlook.com>
2025-12-28 13:33:23 +0530

    Resolve base commits

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUIGeneratorMCP",
+    platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "SwiftUIGeneratorMCP",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+            ]
+        ),
+        .testTarget(
+            name: "SwiftUIGeneratorMCPTests",
+            dependencies: ["SwiftUIGeneratorMCP"]
+        ),
+    ]
+)
